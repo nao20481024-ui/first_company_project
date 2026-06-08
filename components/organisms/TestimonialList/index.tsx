7d1f158686764452f470562d8b1cc7ffd62a45c8
Nao <nao20481024@gmail.com>
2026-06-07 22:56:16 -0400

    fixed compilimg

--- components/organisms/TestimonialList/index.tsx
+++ components/organisms/TestimonialList/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
-import { Pagination } from 'swiper'
+import { Pagination } from 'swiper/modules'
 
 const TestimonialList = () => {
   return (
